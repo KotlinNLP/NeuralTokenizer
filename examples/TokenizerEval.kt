cf688f68985879b4ccaa32710ce0abc99b750a3a
KotlinNLP Authors <github@kotlinnlp.com>
2017-08-04 11:29:37 +0200

    Set ValidationHelper usage

--- examples/TokenizerEval.kt
+++ examples/TokenizerEval.kt
@@ -5,8 +5,6 @@
  * file, you can obtain one at http://mozilla.org/MPL/2.0/.
  * ------------------------------------------------------------------*/
 
-import com.kotlinnlp.conllio.CoNLLUEvaluator
-import com.kotlinnlp.conllio.CoNLLWriter
 import com.kotlinnlp.neuraltokenizer.*
 import java.io.File
 import java.io.FileInputStream
@@ -17,84 +15,24 @@ import java.io.FileInputStream
  */
 fun main(args: Array<String>) {
 
-  val OUTPUT_FILENAME = "/tmp/tokenizer_output_corpus.conll"
-  val TEST_FILENAME = "/tmp/tokenizer_test_corpus.conll"
+  println("Reading dataset...")
 
   val testSet = readDataset(args[0])
 
-  val tokenizer = buildTokenizer(modelFilename = args[1])
-  val outputSentences = tokenizer.tokenize(text = testSet.first.joinToString(""))
+  val tokenizer = NeuralTokenizer(
+    maxSegmentSize = 50,
+    model = NeuralTokenizerModel.load(FileInputStream(File(args[1]))))
 
-  CoNLLWriter.toFile(
-    sentences = outputSentences.toCoNLLSentences(),
-    outputFilePath = OUTPUT_FILENAME,
-    writeComments = false)
+  println("\n-- START VALIDATION OVER %d TEST SENTENCES".format(testSet.size))
 
-  CoNLLWriter.toFile(
-    sentences = buildSentences(testSet).toCoNLLSentences(),
-    outputFilePath = TEST_FILENAME,
-    writeComments = false)
+  val helper = ValidationHelper(tokenizer)
+  val stats: ValidationHelper.EvaluationStats = helper.validate(testSet)
 
-  println(CoNLLUEvaluator.evaluate(systemFilePath = OUTPUT_FILENAME, goldFilePath = TEST_FILENAME))
+  println()
+
+  println("Tokens accuracy     ->   Precision: %.2f%%  |  Recall: %.2f%%  |  F1 Score: %.2f%%"
+    .format(100.0 * stats.tokens.precision, 100.0 * stats.tokens.recall, 100.0 * stats.tokens.f1Score))
+
+  println("Sentences accuracy  ->   Precision: %.2f%%  |  Recall: %.2f%%  |  F1 Score: %.2f%%"
+    .format(100.0 * stats.sentences.precision, 100.0 * stats.sentences.recall, 100.0 * stats.sentences.f1Score))
 }
-
-/**
- *
- */
-private fun buildTokenizer(modelFilename: String) = NeuralTokenizer(
-  maxSegmentSize = 50,
-  model = NeuralTokenizerModel.load(FileInputStream(File(modelFilename))))
-
-/**
- *
- */
-private fun buildSentences(dataset: Pair<ArrayList<String>, ArrayList<ArrayList<Int>>>): ArrayList<Sentence> {
-
-  val sentences = ArrayList<Sentence>()
-  val textLen: Int = 0
-
-  dataset.first.zip(dataset.second).forEachIndexed { sentenceId, (sentence, goldClassification) ->
-
-    sentences.add(Sentence(
-      id = sentenceId,
-      text = sentence,
-      startAt = textLen,
-      endAt = textLen + sentence.length,
-      tokens = buildTokens(sentence = sentence, goldClassification = goldClassification)
-    ))
-  }
-
-  return sentences
-}
-
-/**
- *
- */
-private fun buildTokens(sentence: String, goldClassification: ArrayList<Int>): ArrayList<Token> {
-
-  val tokens = ArrayList<Token>()
-  var startIndex: Int = 0
-
-  goldClassification.forEachIndexed { i, charClass ->
-
-    if (charClass != 2) { // end of token or end of sentence
-      val word: String = sentence.substring(startIndex, i + 1)
-
-      tokens.add(Token(
-        id = tokens.size,
-        form = word,
-        startAt = startIndex,
-        endAt = i,
-        isSpace = word.length == 1 && word.toCharArray()[0].isSpace()))
-
-      startIndex = i + 1
-    }
-  }
-
-  return tokens
-}
-
-/**
- * @return a Boolean indicating if this Char is a spacing character.
- */
-private fun Char.isSpace(): Boolean = Regex("\\s|\\t|\\n|\\r").containsMatchIn(this.toString())
