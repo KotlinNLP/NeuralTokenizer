23d7d5289b4794552f4662f38364edb8c6ccbdde
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-03 13:30:26 +0200

    Add div by zero checks

--- src/main/kotlin/com/kotlinnlp/neuraltokenizer/helpers/ValidationHelper.kt
+++ src/main/kotlin/com/kotlinnlp/neuraltokenizer/helpers/ValidationHelper.kt
@@ -142,8 +142,9 @@ class ValidationHelper(val tokenizer: NeuralTokenizer) {
     val correctDouble: Double = correct.toDouble()
 
     return CoNLLStats( // skip first field (metrics)
-      precision = correctDouble / outputTotal,
-      recall = correctDouble / goldTotal,
-      f1Score = 2 * correctDouble / (outputTotal + goldTotal))
+      precision = if (outputTotal > 0) correctDouble / outputTotal else 0.0,
+      recall = if (goldTotal > 0) correctDouble / goldTotal else 0.0,
+      f1Score = if (outputTotal > 0 && goldTotal > 0) 2 * correctDouble / (outputTotal + goldTotal) else 0.0
+    )
   }
 }
